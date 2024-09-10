a67e63bdddb44a8212986cc6c5a2756c7059d9bd
Igor Kodzhebash <igorkodzebash@gmail.com>
2024-09-10 20:52:12 +0300

    fixed bugs for task2

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,4 +1,4 @@
 // code
 let number = Math.floor((Math.random() * 100));
-number % 2 === 0 ? console.log(number, ':Это четное число') : console.log(number, ':Это нечетное число')
+number % 2 === 0 ? console.log(`${number} ':Это четное число'`) : console.log(`${number} ':Это нечетное число'`)
 
