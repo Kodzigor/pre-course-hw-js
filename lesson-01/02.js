94bc5c5ac98a0061cf2ac3eeaa3171b7ccdca8d1
Igor Kodzhebash <igorkodzebash@gmail.com>
2024-09-11 10:55:54 +0300

    add square brackets surrounding number

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,9 +1,4 @@
 // code
 let number = Math.floor((Math.random() * 100));
 
-
-if(number % 2 === 0) {
-    console.log(`${number} :Это четное число`)
-} else {
-    console.log(`${number} :Это нечетное число`)
-}
+number % 2 === 0 ? console.log(`[${number}] :Это четное число`) :  console.log(`[${number}] :Это нечетное число`) ;
