4ed2ee69caf30e39dd5293e9a36c9fcc0a61eaa7
Igor Kodzhebash <igorkodzebash@gmail.com>
2024-09-23 18:36:28 +0300

    renamed number

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -1,6 +1,6 @@
-let number1 = 1;
-console.log(number1++);
-console.log(++number1);
-console.log(number1--);
-console.log(--number1);
+let number = 1;
+console.log(number++);
+console.log(++number);
+console.log(number--);
+console.log(--number);
 
